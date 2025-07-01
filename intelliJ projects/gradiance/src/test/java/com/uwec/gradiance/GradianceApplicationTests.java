6fd9f1818ee18b418efa980d9019cc9c9977af16
Sam Zien <144245827+ziens-uwec@users.noreply.github.com>
2025-07-01 15:20:29 -0500

    Spring Boot Project Upload
    upload IntelliJ version of spring boot project with code matching what's on the server now. untested deploy.yml included.

---
+++ intelliJ projects/gradiance/src/test/java/com/uwec/gradiance/GradianceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uwec.gradiance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradianceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
