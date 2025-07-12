fab8ceff0fe1b62e39f924e153ae91fdf6fa7181
eli-miller2822 <166755259+eli-miller2822@users.noreply.github.com>
2025-07-11 20:36:02 -0500

    Delete demo directory

--- demo/src/main/java/com/Demo/webAppDemo2/demo/dao/gradienceDao.java
+++
@@ -1,9 +0,0 @@
-package com.Demo.webAppDemo2.demo.dao;
-
-import com.Demo.webAppDemo2.demo.model.Student;
-
-import java.util.List;
-
-public interface gradienceDao {
-    List<Student> findAll();
-}
